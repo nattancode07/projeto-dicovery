45c48ccd9ca974b7e0c3c0ac0680a27861372e8f
Elyaquim Nattan <elyaquimnattan@gmail.com>
2023-08-24 12:39:36 -0300

    colocando-minha-foto

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function toggleMode(){
         const img = document.querySelector("#profile img")
 
        if(html.classList.contains('light')){
-        img.setAttribute('src','assets/avatar-light.png' )
+        img.setAttribute('src','assets/avatar-light.jpg' )
        } else{
-        img.setAttribute("src","assets/avatar.png")
+        img.setAttribute("src","assets/avatar.jpg")
        }
 
        //pegar a tag img
